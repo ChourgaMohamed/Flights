db597a9ff9085e10be60fd74f133574efd890388
Mohamed Chourga <m.chourga@student.vu.nl>
2025-03-22 17:06:36 +0100

    Flight_Statistics updated

--- flights_project/pages/4_🧮_Flight_Statistics.py
+++ flights_project/pages/4_🧮_Flight_Statistics.py
@@ -1,5 +1,6 @@
 import streamlit as st
-from part3 import flight_statistics, flight_analysis
+from part1 import plot_routes
+from part3 import flight_analysis
 from db import get_db_connection
 import pandas as pd
 import plotly.graph_objects as go
@@ -7,27 +8,26 @@ from flights_project import utils
 
 db_conn = get_db_connection()
 
-col1, col2 = st.columns([2, 3])
-total = flight_statistics.get_total_flights(conn=db_conn)
-busiest = flight_statistics.get_busiest_airports(conn=db_conn)
-busiest_df = pd.DataFrame(busiest, columns=["Airport", "Flights"])
-with col1:
-     st.title("Flight statistics")
-     total = flight_statistics.get_total_flights(conn=db_conn)
-     st.dataframe(busiest_df.set_index("Airport"))
-     st.write(f"Total flights: {total}")
-with col2:
-    #Pie chart
-    pie_colors = [
-    utils.COLOR_PALETTE["pakistan_green"],
-    utils.COLOR_PALETTE["light_green"],
-    utils.COLOR_PALETTE["nyanza"]
-]
-    fig = go.Figure(data=[go.Pie(labels=busiest_df["Airport"], values=busiest_df["Flights"], hole=0.4,
-            marker=dict(colors=pie_colors))])
-    fig.update_layout(title_text="Busiest airports by flight volume")
-    # Render the chart
+# Load airports data
+st.title("Flight statistics")
+airports_df = utils.load_airports_data()
+placeholder = "Select an airport"
+st.sidebar.header("Select departure airport")
+dep_airport = st.sidebar.selectbox("Departure airport", [placeholder]+["JFK", "LGA", "EWR"], index=0)
+#Sidebar one day delay analysis
+st.sidebar.header("Select a day")
+flight_date = st.sidebar.date_input("Day", value=pd.to_datetime("2023-01-01"))
+
+# Convert dates to string format
+flight_date_str = flight_date.strftime("%Y-%m-%d")
+
+if dep_airport != placeholder:
+    fig, fig2 = plot_routes.plot_multiple_routes(dep_airport, flight_date_str, airports_df, conn= db_conn)
+    fig.update_layout(margin=dict(l=0, r=0, t=20, b=0)) 
     st.plotly_chart(fig, use_container_width=True)
+    st.plotly_chart(fig2, use_container_width=True)
+else:
+    st.write("Please select a valid departure airport.")
 
 st.subheader("Distance verification analysis")
 fig, text1, text2 = flight_analysis.verify_distance_computation(conn=db_conn)
